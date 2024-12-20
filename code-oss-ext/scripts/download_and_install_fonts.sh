4a335cd42a36d5dcc8a166ab0b400eb4199a8d83
Michael Bychkowski <mbychkowski@google.com>
2024-12-20 12:58:05 -0500

    Feat/Update workstation image (#1)
    Update workstation image for vscode

---
+++ code-oss-ext/scripts/download_and_install_fonts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export FONTS_HACK_VERSION="v3.3.0"
+export FONTS_HACK_NAME="Meslo"
+export FONTS_PATH="/usr/share/fonts/truetype/${FONTS_HACK_NAME}"
+export FONTS_HACK_ZIP="${FONTS_HACK_NAME}.zip"
+
+if [ ! -d "${FONTS_PATH}" ]; then
+  echo "Making fonts directory"
+
+  # Font directory path
+  mkdir -p "${FONTS_PATH}"
+
+else
+  echo "Fonts directory already exists"
+
+fi
+
+curl --fail --location --show-error https://github.com/ryanoasis/nerd-fonts/releases/download/${FONTS_HACK_VERSION}/${FONTS_HACK_ZIP} --output ${FONTS_HACK_ZIP}
+unzip -o -q -d ${FONTS_PATH} ${FONTS_HACK_ZIP}
+rm ${FONTS_HACK_ZIP}
+
+echo "fc-cache -fv"
+fc-cache -fv
