b1ce2d813506fdceb0eb35930a3b1f27af656089
Michael Bychkowski <mbychkowski@google.com>
2026-04-06 23:43:15 +0000

    chore(code-oss-ai): clean up settings provision script to be simpler and drop unused bash configs since we use zsh natively

--- code-oss-ai/configs/.bash_aliases
+++
@@ -1,8 +0,0 @@
-# Any additional aliases that you would like to add to .bashrc
-
-if [ -f ~/.ssh ]; then
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/*
-fi
-
-eval "$(starship init bash)"
--- code-oss-ai/scripts/configure_workstation_settings.sh
+++ code-oss-ai/scripts/configure_workstation_settings.sh
@@ -2,42 +2,23 @@
 
 echo "Setup user settings"
 
-export PATH=$PATH:/usr
 export HOME_DIR=/home/user
-export CODEOSS_CONFIG_PATH="$HOME_DIR/.codeoss-cloudworkstations"
-export CODEOSS_USER_CONFIG_PATH="$HOME_DIR/.config"
-export CODEOSS_SETTINGS_CONFIG_PATH="$CODEOSS_CONFIG_PATH/data/Machine"
+# Use the correct internal path for VS Code OSS Settings
+export CODEOSS_SETTINGS_DIR="$HOME_DIR/.codeoss-cloudworkstations/data/Machine"
+export USER_CONFIG_DIR="$HOME_DIR/.config"
 
 if [ -f "/tmp/.codeoss-configs/starship.toml" ]; then
-  mkdir -p $CODEOSS_USER_CONFIG_PATH
-
-  mv /tmp/.codeoss-configs/starship.toml $CODEOSS_USER_CONFIG_PATH/
+  mkdir -p $USER_CONFIG_DIR
+  mv /tmp/.codeoss-configs/starship.toml $USER_CONFIG_DIR/
+  chown -R user:user $USER_CONFIG_DIR
+  chmod -R 755 $USER_CONFIG_DIR
 fi
 
 if [ -f "/tmp/.codeoss-configs/settings.json" ]; then
-  mkdir -p $CODEOSS_SETTINGS_CONFIG_PATH
-
-  mv /tmp/.codeoss-configs/settings.json $CODEOSS_SETTINGS_CONFIG_PATH/
-fi
-
-if [ -f "/tmp/.codeoss-configs/.bash_aliases" ]; then
-  mv /tmp/.codeoss-configs/.bash_aliases $HOME_DIR/
-
-  chown user:user $HOME_DIR/.bash_aliases
-  chmod 755 $HOME_DIR/.bash_aliases
-fi
-
-if [ -f "/tmp/.codeoss-configs/.profile" ]; then
-  mv /tmp/.codeoss-configs/.profile $HOME_DIR/
-
-  chown user:user $HOME_DIR/.profile
-  chmod 755 $HOME_DIR/.profile
+  mkdir -p $CODEOSS_SETTINGS_DIR
+  mv /tmp/.codeoss-configs/settings.json $CODEOSS_SETTINGS_DIR/
+  chown -R user:user "$HOME_DIR/.codeoss-cloudworkstations"
+  chmod -R 755 "$HOME_DIR/.codeoss-cloudworkstations"
 fi
 
 rm -rf /tmp/.codeoss-configs
-
-chown -R user:user $CODEOSS_USER_CONFIG_PATH
-chmod -R 755 $CODEOSS_USER_CONFIG_PATH
-
-chown -R user:user $CODEOSS_CONFIG_PATH
-chmod -R 755 $CODEOSS_CONFIG_PATH
